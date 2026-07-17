834d3e533feca3f080f507bd54271df91dafe658
16407-oss <16407@phanthong.ac.th>
2026-07-17 15:46:32 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
+function createStar() {
 
+    const star = document.createElement("div");
+
+    star.className = "star";
+
+    star.innerHTML = Math.random() > 0.5 ? "⭐" : "💖";
+
+    star.style.left = Math.random() * 100 + "vw";
+
+    star.style.animationDuration = (3 + Math.random() * 3) + "s";
+
+    document.body.appendChild(star);
+
+    setTimeout(() => {
+        star.remove();
+    }, 6000);
+
+}
+
+setInterval(createStar, 300);
